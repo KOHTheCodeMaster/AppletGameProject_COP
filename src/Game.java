a78c1069c31176087a69dbf057a351366416af30
KOHTheCodeMaster <kohthecodemaster02@gmail.com>
2018-05-27 17:40:40 +0530

    5th Commit. Commit made to Local Branch "initialBranch-L-1" Updated Game Class.     paintComponent() method updated.     * Added Red Ellipse Ball.     * Added Cyan RoundRect Bat.

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.RoundRectangle2D;
+
+import static java.awt.RenderingHints.VALUE_ANTIALIAS_ON;
 
 public class Game extends JComponent {
 
@@ -7,9 +11,16 @@ public class Game extends JComponent {
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
 
-        g2.setColor(Color.black);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
 
+        g2.setColor(Color.black);
         g2.fillRect(0, 0, getWidth(), getHeight());
+
+        g2.setColor(Color.red);
+        g2.fill(new Ellipse2D.Double(100, 100, 15, 15));
+
+        g2.setColor(Color.cyan);
+        g2.fill(new RoundRectangle2D.Double(200, 200, 100, 10, 20, 20));
 
     }
 }
