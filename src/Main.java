f98c424c0ea771f3c01736a9ae92f396cb645586
KOHTheCodeMaster <39646458+KOHTheCodeMaster@users.noreply.github.com>
2018-05-27 16:32:07 +0530

    3rd Commit. Updated Main Class with 4 Basic Empty methods for Applet: init() start() stop() destroy()

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-public class Main {
+import javax.swing.*;
+
+public class Main extends JApplet {
+
+    @Override
+    public void init() {
+        System.out.println("Init Method Called.");
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Start Method Called.");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stop Method Called.");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Destroy Method Called.");
+    }
 }
